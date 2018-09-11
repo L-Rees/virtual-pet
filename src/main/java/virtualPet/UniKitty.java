1cecc32f31b7b706d5a6740e8b917ee1a2b616c7
Lisa Rees <kortirion@gmail.com>
2018-09-11 15:04:53 -0400

    add interface, reactions, art

--- src/main/java/virtualPet/UniKitty.java
+++ src/main/java/virtualPet/UniKitty.java
@@ -43,10 +43,10 @@ public class UniKitty {
 
 	public UniKitty() {
 		this.name = "UniKitty";
-		this.beautyLevel = 4;
-		this.hungerLevel = 4;
-		this.positivityLevel = 4;
-		this.boredomLevel = 1;
+		this.beautyLevel = 2;
+		this.hungerLevel = 1;
+		this.positivityLevel = 2;
+		this.boredomLevel = 2;
 	}
 
 	public void feed() {
@@ -55,11 +55,12 @@ public class UniKitty {
 		System.out.println("2 a rainbow sparkle cupcake");
 		System.out.println("3 a mouse");
 		String foodChoice = input.nextLine();
+		System.out.println("\n\n\n");
 		if (foodChoice.equals("1")) {
 			beBored();
 			beHungry();
 		} else if (foodChoice.equals("2")) {
-			hungerLevel -= 3;
+			hungerLevel -= 5;
 			beVain();
 		} else {
 			hungerLevel += 1;
@@ -74,15 +75,22 @@ public class UniKitty {
 		bePrimped();
 		beautyLevel += 3;
 		positivityLevel += 2;
-		boredomLevel -= 2;
+		boredomLevel -= 3;
 	}
 
 	public void tick() {
+
 		hungerLevel += 2;
 		if ((int) (Math.random() * 3) + 1 == 1) {
+			System.out.println("Unikitty decided to do a little primping!");
 			primping();
 		} else {
-			boredomLevel += 2;
+			boredomLevel += 1;
+		}
+		try {
+			Thread.sleep(3500);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
 		}
 	}
 
@@ -95,6 +103,7 @@ public class UniKitty {
 		System.out.println("5 Good UniKitty!");
 		System.out.println("6 I think you're almost as cool as Emmett.");
 		String compliment = input.nextLine();
+		System.out.println("\n\n\n");
 
 		if (compliment.equals("1") || compliment.equals("4")) {
 			beVain();
@@ -109,7 +118,8 @@ public class UniKitty {
 			positivityLevel -= 3;
 			boredomLevel += 3;
 		} else if (compliment.equals("6")) {
-			beUnPositive();
+			beAngry();
+			positivityLevel -= 4;
 		} else {
 			beBored();
 			boredomLevel += 3;
@@ -118,104 +128,174 @@ public class UniKitty {
 	}
 
 	public void reaction() {
+		if (gethungerLevel() == 6) {
+			System.out.println("I'm hungry!");
+			beUnPositive();
+		} else if (gethungerLevel() == 7) {
+			System.out.println("I'm getting hungrier!");
+			beAngry();
+		} else if (gethungerLevel() == 8 || gethungerLevel() == 9) {
+			System.out.println("You'd better feed me!");
+			beHungry();
+			beVeryAngry();
+		} else if (gethungerLevel() >= 10) {
+			beRunningAway();
+		} else if (getBoredomLevel() > 7) {
+			beBored();
+		} else if (getBeautyLevel() > 10) {
+			bePrimped();
+		} else if (getPositivityLevel() > 10) {
+			bePretty();
+		}
 
-		
-		
-		
-		
 	}
-	
+
 	public String showPositivity() {
-		return ("Positivity:\nBad-" + showLevel(getPositivityLevel()) + "-Just Great\n");
+		return ("Positivity:\n    Bad-" + showLevel(getPositivityLevel()) + "-Just Great\n");
 	}
-	
+
 	public String showHunger() {
-		return ("Hunger:\nGood-" + showLevel(gethungerLevel())) + "-Starving\n";
+		return ("Hunger:\n   Good-" + showLevel(gethungerLevel())) + "-Starving\n";
 	}
-	
+
 	public String showBoredom() {
 		return ("Boredom:\nAwesome-" + showLevel(getBoredomLevel())) + "-Yawnsville\n";
 	}
+
 	public String showBeauty() {
-		return ("Beauty:\nUggo-" + showLevel(getBeautyLevel())) + "-An angel\n";
+		return ("Beauty:\n   Uggo-" + showLevel(getBeautyLevel())) + "-An angel\n";
 	}
-	
+
 	public String showLevel(int quality) {
 		String level = "";
-		for (int i = 0; i < quality; i++ ) {
+		for (int i = 0; i < quality; i++) {
 			level = level + "|";
 		}
-		for (int i = 0; i < 10-quality; i++) {
+		for (int i = 0; i < 10 - quality; i++) {
 			level = level + ".";
 		}
 		return level;
-				
-				//  Bad-||||......-Great!
+
+		// Bad-||||......-Great!
 	}
 
 	public void getStatus() {
-		System.out.println(showPositivity() + showHunger() + showBeauty() +  showBoredom());
+		System.out.println(
+				"UNIKITTY STATUS:\n" + showPositivity() + showBeauty() + "\n\n" + showHunger() + showBoredom());
 	}
 
+	String asciiName = "  _    _       _ _    _ _   _         \n" + " | |  | |     (_) |  (_) | | |        \n"
+			+ " | |  | |_ __  _| | ___| |_| |_ _   _ \n" + " | |  | | '_ \\| | |/ / | __| __| | | |\n"
+			+ " | |__| | | | | |   <| | |_| |_| |_| |\n" + "  \\____/|_| |_|_|_|\\_\\_|\\__|\\__|\\__, |\n"
+			+ "                                 __/ |\n" + "                                |___/ \n";
+
+	public void intro() {
+		System.out.println(asciiName);
+		bePretty();
+		getStatus();
+	}
+
+	String catBike = "                 ________________ \n" + "                | You're NOT my  |_____    __ \n"
+			+ "                | friend anymore |     |__|  |_________ \n"
+			+ "     /\\         |________________| Good|::|  |        / \n"
+			+ "   /\\||/\\       |                \\.____|::|__| BYE  <	\n"
+			+ "  ( o_o  )_     |                      \\::/  \\._______\\ \n" + "   (u--u   \\_)  | \n"
+			+ "    (||___   )==\\ \n" + "  ,dP\"/b/=( /P\"/b\\ \n" + "  |8 || 8\\=== || 8 \n" + "  `b,  ,P  `b,  ,P \n"
+			+ "    \"\"\"`     \"\"\"` \n";
+
 	public void beOffended() {
-		// ASCII art
-		System.out.println("I can't believe you think I'd appreciate that! As if!");
+
+		String saying = "I can't believe you think I'd appreciate that! As if!";
+		String offendedCat = "      /\\   \n" + "    /\\|| /\\ \n" + "   (       ) \n" + "   /  >.<  \\ \n"
+				+ "   \\__ 0 __/   " + saying + "\n" + "     /   \\  /\n" + "    / ___ \\/ \n" + "    \\/___\\/ \n";
+		System.out.println(offendedCat);
 	}
 
 	public void beInsulted() {
-		// art
-		System.out.println("I can't even look at you right now! How could you say that to me?!");
+
+		String saying = "I can't even look at you right now! How could you say that to me?!";
+		String insultedCat = "      /\\   \n" + "    /\\|| /\\ \n" + "   ( <o o> ) \n" + "   /  >.<  \\ \n"
+				+ "   \\__ O __/  " + saying + "\n" + "     /   \\  /\n" + "    / ___ \\/ \n" + "    \\/___\\/ \n";
+		System.out.println(insultedCat);
 	}
 
 	public void beHungry() {
-		// ASCII art
-		System.out.println("My tummy is so rumbly! It's making me think unpositive thoughts! That's just a drag!");
+
+		String saying = "My tummy is so rumbly! It's making me think unpositive thoughts!";
+		String hungryCat = "      /\\   \n" + "    /\\|| /\\ \n" + "   (  -  - ) \n" + "   /  >.<  \\ \n"
+				+ "   \\__ ~~__/  " + saying + "\n" + "     /   \\  /\n" + "    / ___ \\/ \n" + "    \\/___\\/ \n";
+		System.out.println(hungryCat);
 	}
 
 	public void beBored() {
-		// art
-		System.out.println("Meow? Sigh.");
+
+		String saying = "Meow? Sigh.";
+		String boredCat = "      /\\   \n" + "    /\\|| /\\ \n" + "   ( <o o> ) \n" + "   /  >o<  \\ \n"
+				+ "   \\__ ~ __/   " + saying + "\n" + "     /   \\  /\n" + "    / ___ \\/ \n" + "    \\/___\\/ \n";
+		System.out.println(boredCat);
 	}
 
-	public void bePretty() {
-		// ASCII art
-		System.out.println("Sometimes I think I could spend ALL DAY just looking at myself in the mirror!");
+	public static void bePretty() {
+
+		String saying = "Sometimes I think I could spend ALL DAY just looking at myself in the mirror!";
+		String prettyCat = "      /\\   \n" + "    /\\|| /\\ \n" + "   (  . .  ) \n" + "   /  >.<  \\ \n"
+				+ "   \\__\\_/__/  " + saying + "\n" + "     /   \\  / \n" + "    / ___ \\/ \n" + "    \\/___\\/ \n";
+		System.out.println(prettyCat);
 	}
 
 	public void bePrimped() {
-		// ASCII art
-		System.out.println(
-				"I'm so naturally lovely and charming, it's hard work to do myself justice.\nLuckily I'm also a natural genius!");
+
+		String saying1 = "I'm so naturally lovely and charming, it's hard work to do myself justice.";
+		String saying2 = "Luckily I'm also a natural genius!";
+
+		String primpedCat = "      /\\   \n" + " *  /\\|| /\\  *  \n" + "   (  _  _ ) \n" + "   /  ___  \\   * "
+				+ saying1 + "\n" + "   \\__\\_/__/ " + saying2 + "\n" + "  *  /   \\  /\n" + "    / ___ \\/   *\n"
+				+ " *  \\/___\\/  *\n";
+		System.out.println(primpedCat);
 	}
 
 	public void beUnPositive() {
-		// ASCII art
-		System.out.println(
-				"It's hard work being me, and you're not helping matters any! I'm losing my natural positivity!");
+
+		String saying = "It's hard work being me, and you're not helping matters any! I'm losing my natural positivity!";
+		System.out.println(saying);
 	}
 
 	public void beVain() {
-		// ASCII art
-		System.out.println("I'm glad you recognize even a little of how amazing I truly am!");
+
+		String saying = "I'm glad you recognize even a little of how amazing I truly am!";
+		String vainCat = "      /\\   \n" + "    /\\|| /\\ \n" + "   (  ^ ^  ) \n" + "   /   }o{ \\ \n"
+				+ "   \\__   __/   " + saying + "\n" + "     /   \\  /\n" + "    / ___ \\/ \n" + "    \\/___\\/ \n";
+		System.out.println(vainCat);
 	}
 
 	public void beAngry() {
-		// ASCII art
-		System.out.println(
-				"I'm just so angry with you right now! Why are you such a meanie?? This pout is going to give me wrinkles!");
+
+		String saying = "I'm just so angry with you right now! Why are you such a meanie??";
+		String angryCat = "      /\\   \n" + "    /\\|| /\\ \n" + "   (  \\ /  ) \n" + "   /  o,o  \\ \n"
+				+ "   \\__~~~__/  " + saying + "\n" + "     /   \\  /\n" + "    / ___ \\/ \n" + "    \\/___\\/ \n";
+		System.out.println(angryCat);
 	}
 
 	public void beVeryAngry() {
-		// ASCII art
-		System.out.println(
-				"I don't think I can take much more of this terrible treatment! \nYou'd better shape up, mister, or I'll have to take things into my own hands!");
+
+		String saying = "I don't think I can take much more of this terrible treatment!";
+		String saying2 = "You'd better shape up, mister, or I'll have to take things into my own hands!";
+		String veryAngryCat = "      /\\   \n" + "    /\\|| /\\ \n" + "   (  \\ /  ) \n" + "   /  o,o  \\ \n"
+				+ "   \\__~~~__/   " + saying + "\n" + "     /   \\  /  " + saying2 + "\n" + "    / ___ \\/ \n"
+				+ "    \\/___\\/ \n";
+		System.out.println(veryAngryCat);
 	}
 
 	public void beRunningAway() {
-		// ASCII art
+
 		System.out.println("I think I've been very patient with you, but this is the last straw! I'm leaving!");
+		System.out.println(catBike);
 		System.out.println("\n\n\n\n\n\nUniKitty has run away. I hope you're proud of yourself.");
 		System.exit(0);
 	}
 
+	public void beExiting() {
+		System.out.println(catBike);
+		System.exit(0);
+	}
 }
--- src/main/java/virtualPet/UniKittyApp.java
+++ src/main/java/virtualPet/UniKittyApp.java
@@ -8,10 +8,29 @@ public class UniKittyApp {
 		UniKitty unikitty = new UniKitty();
 		Scanner input = new Scanner(System.in);
 		
+		unikitty.intro();
+		String menuChoice = "";
 		
-		unikitty.getStatus();
-		System.out.println("What would you like to do with UniKitty?");
-	
+		while (!menuChoice.equals("4")) {
+			System.out.println("What would you like to do with UniKitty?");
+			System.out.println("1 Compliment UniKitty");
+			System.out.println("2 Offer UniKitty a snack");
+			System.out.println("3 Do nothing but stare at UniKitty's loveliness");
+			System.out.println("4 Exit the game and leave UniKitty homeless in the digital void");
+			menuChoice = input.nextLine();
+			System.out.println("\n\n\n");
+			if (menuChoice.equals("1")) {
+				unikitty.compliment();
+			}else if (menuChoice.equals("2")) {
+				unikitty.feed();
+			}
+			
+			unikitty.tick();
+			unikitty.reaction();
+			unikitty.getStatus();
+		}
+		unikitty.beExiting();
+		input.close();
 	}
 
 }
